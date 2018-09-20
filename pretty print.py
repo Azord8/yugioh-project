e59be7515f08361afd2b1c7bed434d1a31ea14f4
Azord8 <joshmoorer@gmail.com>
2018-09-20 17:53:01 -0500

    formatted JSOn for readability

--- pretty print.py
+++ pretty print.py
@@ -3,25 +3,31 @@ import os
 import pprint
 
 
-for root, dirs, files in os.walk("/home/azord/PycharmProjects/ygo project/sets/"):
-    for file in files:
-        if "_pretty" in file:
-            oldfile = open("/home/azord/PycharmProjects/ygo project/sets/" + file)
-            newfile = open(file, "w")
-            for line in oldfile:
-                if '\'numbers\':' not in line:
-                    newfile.write(line)
+# for root, dirs, files in os.walk("/home/azord/PycharmProjects/ygo project/sets/"):
+#     for file in files:
+#         oldfile = open("/home/azord/PycharmProjects/ygo project/sets/" + file)
+#         newfile = open(file + "2", "w")
+#         cards = False
+#         for line in oldfile:
+#             if not cards:
+#                 if '"cards":' not in line:
+#                     newfile.write(line)
+#                 else:
+#                     newfile.write("[")
+#                     cards = True
 
 
 
+# for root, dirs, files in os.walk("sets/"):
+#     for file in files:
+#         json_data = open("sets/" + file).read()
+#         data = json.loads(json_data)
+#         f = open((file + "_pretty"), "w")
+#         f.write(json.dumps(data, indent=4))
 
+with open('pretty sets/Crossroads of Chaos_pretty') as data_file:
+    setlist = json.load(data_file)
 
-        # json_data = open("/home/azord/PycharmProjects/ygo project/sets/" + file).read()
-        # data = json.loads(json_data)
-        # f = open((file + "_pretty"), "w")
-        # f.write(pprint.pformat(data, indent=1, width=100, depth=5, compact=False))
-
-
-
-
+for element,data in setlist:
+    print(data)
 
