22c92796c03688ed10eba81ebeefce6e5ec9eb2f
Azord8 <joshmoorer@gmail.com>
2018-09-21 12:04:15 -0500

    finalized and flattened json files

--- pretty print.py
+++ pretty print.py
@@ -1,21 +1,36 @@
 import json
 import os
-import pprint
+from collections import OrderedDict
 
 
-# for root, dirs, files in os.walk("/home/azord/PycharmProjects/ygo project/sets/"):
-#     for file in files:
-#         oldfile = open("/home/azord/PycharmProjects/ygo project/sets/" + file)
-#         newfile = open(file + "2", "w")
-#         cards = False
-#         for line in oldfile:
-#             if not cards:
-#                 if '"cards":' not in line:
-#                     newfile.write(line)
-#                 else:
-#                     newfile.write("[")
-#                     cards = True
+for root, dirs, files in os.walk("pretty sets/"):
+    for file in files:
+        oldfile = open("pretty sets/" + file)
+        newfile = open("final sets/" + file, "w")
+        numbers = False
+        count = 0
+        first = False
+        for line in oldfile:
+            if not numbers:
+                if '"numbers": [' not in line:
+                    newfile.write(line)
+                else:
+                    numbers = True
+            else:
+                if not first:
+                    first = True
 
+                else:
+                    if "}" in line:
+                        if count != 3:
+                            count += 1
+                            newfile.write(line)
+                    elif count == 3:
+                        count = 0
+                        numbers = False
+                        first = False
+                    else:
+                        newfile.write(line)
 
 
 # for root, dirs, files in os.walk("sets/"):
@@ -25,9 +40,16 @@ import pprint
 #         f = open((file + "_pretty"), "w")
 #         f.write(json.dumps(data, indent=4))
 
-with open('pretty sets/Crossroads of Chaos_pretty') as data_file:
-    setlist = json.load(data_file)
+# for root, dirs, files in os.walk("sets/"):
+#     for file in files:
+#         with open('sets/' + file) as data_file:
+#             data = json.load(data_file, object_pairs_hook=OrderedDict)
+#             for key, value in data.items():
+#                 if key == "data":
+#                     for key2, value2 in value.items():
+#                         if key2 == "cards":
+#                             f = open("pretty sets/" + file + "_pretty", "w")
+#                             f.write(json.dumps(value2, indent=4))
 
-for element,data in setlist:
-    print(data)
+
 
