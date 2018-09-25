2fd599691da9b0486ee00f5b1202762c081b2d6b
Azord8 <joshmoorer@gmail.com>
2018-09-25 17:02:38 -0500

    finding shortnames and release dates in preparation for database upload

--- Pack Check.py
+++ Pack Check.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-for root, dirs, files in os.walk("pretty sets/"):
+for root, dirs, files in os.walk("final sets/"):
     for file in files:
         data = open("final sets/" + file)
         dataset = json.load(data)
